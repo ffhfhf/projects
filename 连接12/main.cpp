9fea94cf2dd32d4d9d62484b5d208f0f7e455471
cg1234567 <1760016246@qq.com>
2017-01-10 09:54:37 +0800

    连接菜单
    已连接好菜单

--- 连接12/main.cpp
+++ 连接12/main.cpp
@@ -339,5 +339,68 @@ void prinluyou()//生成路由表
    }
     cout<<"------------------------------------"<<endl;
 }
+int main()
+{
+    int weight;
+    int chose;
+    primary1();
+    primary2();
+    cout<<"                         ============================ "<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"                               最佳路由选择           "<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"                         ============================="<<endl;
+    cout<<"                                 1 结点管理           "<<endl;
+    cout<<"                                 2 边管理             "<<endl;
+    cout<<"                                 3 路由表输出         "<<endl;
+    cin>>weight;
+    while(1)
+    {
+    switch(weight)
+    {
+    case 1:
+          cout<<"1 增加结点 2删除结点"<<endl;
+          cin>>chose;
+          if(chose==1)
+          {
+             operaddpo();
+          }
+          else
+          {
+              operdepo();
+          }
+          break;
+    case 2:
+           cout<<"1 增加边 2删除边"<<endl;
+           cin>>chose;
+           if(chose==1)
+           {
+               operaddpa();
+           }
+           else
+           {
+               operdepa();
+           }
+          break;
+    case 3:
+        prinluyou();
+        break;
+    }
+    cout<<"终止程序输入3"<<endl;
+    cin>>chose;
+    if(chose==3)
+    {
+        break;
+    }
+    cout<<"                         ============================="<<endl;
+    cout<<"                                 1 结点管理           "<<endl;
+    cout<<"                                 2 边管理             "<<endl;
+    cout<<"                                 3 路由表输出         "<<endl;
+    cout<<"                         ============================="<<endl;
+    cin>>weight;
+  }
+}
 
 
