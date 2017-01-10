dc8241e42da1a80a161ab57561ae33bf5b704afd
cg1234567 <1760016246@qq.com>
2017-01-10 09:49:54 +0800

    已连接
    连接好模块3

--- 模块2/main.cpp
+++ 模块2/main.cpp
@@ -3,7 +3,6 @@
 #include<string>
 
 using namespace std;
-
 #define maxvalue 100
 int n=9;
 int dist[100];
@@ -22,6 +21,53 @@ struct link1
     link2*link=NULL;
 };
 link1 main1[100];
+void primary1()//初始化路由结点
+{
+    string s;
+    int i=0;
+    fstream out("e://14-李浩//1.txt");
+    for(i=0;i<9;i++)
+    {
+        out>>main1[i].i;
+        out>>main1[i].s;
+        main1[i].flag=0;
+    }
+}
+void primary2()//初始化结点的关联结点
+{
+    link2*temp,*current;
+    int sum;
+    int i;
+    fstream out1("e://14-李浩//2.txt");
+    char s;
+    string gh;
+    for( i=0;i<9;i++)
+    {
+        temp=new link2;
+        out1>>gh;
+        temp->i=gh;
+        out1>>s;
+        sum=s-'0';
+        temp->j=sum;
+        main1[i].link=temp;
+        current=temp;
+        while(1)
+        {
+            out1>>gh;
+            if(gh[0]=='#')
+            {
+                break;
+            }
+            temp=new link2;
+            temp->i=gh;
+            out1>>s;
+            sum=s-'0';
+            temp->j=sum;
+            current->link=temp;
+            current=current->link;
+        }
+    }
+}
 void operdepo()//删除结点操作
 {
   string i;
--- 连接12/main.cpp
+++ 连接12/main.cpp
@@ -231,5 +231,113 @@ void operaddpa()//增加边
     temp->j=gh;
     current->link=temp;
 }
+int getweight(int i,int j)//取权值
+{
+  if(i==j)
+  {
+      return 0;
+  }
+  if(main1[i].flag==1)
+  {
+      return maxvalue;
+  }
+  if(main1[j].flag==1)
+  {
+      return maxvalue;
+  }
+  link2 *current=main1[i].link;
+  while(current!=NULL)
+  {
+      if(current->i==main1[j].i)
+      {
+          break;
+      }
+      current=current->link;
+  }
+  if(current==NULL)
+  {
+      return maxvalue;
+  }
+  return current->j;
+}
+void prinluyou()//生成路由表
+{
+   string g;
+   cout<<"输入要生成的路由表的名称"<<endl;
+   cin>>g;
+   int count1;
+   for(count1=0;count1<n;count1++)
+   {
+       if(main1[count1].i==g)
+       {
+           break;
+       }
+   }
+   bool *s=new bool[n];
+   int i,j,k;
+   int min1,w;
+   for(i=0;i<n;i++)
+   {
+       dist[i]=getweight(count1,i);
+       s[i]=false;
+       if(i!=count1&&dist[i]<min1)
+       {
+           path[i]=count1;
+       }
+       else
+       {
+           path[i]=-1;
+       }
+   }
+   s[count1]=true;
+   dist[count1]=0;
+   for(i=0;i<n-1;i++)
+   {
+       min1=maxvalue;
+       int u=count1;
+       for(j=0;j<n;j++)
+       {
+           if(s[j]==false&&dist[j]<maxvalue)
+           {
+               u=j;
+               min1=dist[j];
+           }
+           s[u]=true;
+           for(k=0;k<n;k++)
+           {
+               w=getweight(u,k);
+               if(s[k]==false&&w<maxvalue&&dist[u]+w<dist[k])
+               {
+                   dist[k]=dist[u]+w;
+                   path[k]=u;
+               }
+           }
+       }
+   }
+   cout<<"------------------------------------"<<endl;
+   cout<<"      目的ip               下一跳   "<<endl;
+   for(k=0;k<n;k++)
+   {
+       if(k==count1)
+       {
+           continue;
+       }
+       if(path[k]==-1)
+       {
+           continue;
+       }
+       if(main1[k].flag==1)
+       {
+           continue;
+       }
+       j=k;
+       while(path[j]!=count1)
+       {
+           j=path[j];
+       }
+    cout<<"     "<<main1[k].s<<"                  "<<main1[j].i<<endl;
+   }
+    cout<<"------------------------------------"<<endl;
+}
 
 
